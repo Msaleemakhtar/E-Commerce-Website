d03ca08014aa382544e0ff08a535e97a98746e9f
Muhammad saleem <saleemakhtar864@gmail.com>
2023-06-12 08:15:15 -0700

    updated

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,7 +2,7 @@ import BASE_PATH_FORAPI from "@/components/shared/BasePath";
 import AllproductsData from "@/components/views/Allproducts";
 
 const fetchAlldata = async () => {
-  const res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=10`, {
+  const res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=4`, {
     next: {
       revalidate: 120,
     },
