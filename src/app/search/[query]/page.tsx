89c9e994534c6f778165fe3ba9cdd5f1ac9b4896
Muhammad saleem <saleemakhtar864@gmail.com>
2023-06-13 08:01:11 -0700

    updated

--- src/app/search/[query]/page.tsx
+++ src/app/search/[query]/page.tsx
@@ -1,5 +1,5 @@
 
-import {oneProductType} from "@/components/utils/ProductDataTyoes"
+import {oneProductType} from "@/components/utils/ProductDataTypes"
 import {client} from "../../../../sanity/lib/client"
 import CardAll from "@/components/views/CardsAll";
 
