6e2ce91c6c649fad3490ca30d95b1c28ebd95bea
Muhammad saleem <saleemakhtar864@gmail.com>
2023-06-14 09:59:14 -0700

    product detail added

--- src/app/catalog/[slug]/page.tsx
+++ src/app/catalog/[slug]/page.tsx
@@ -1,10 +1,45 @@
+import { Metadata } from "next";
+import {
+  oneProductType,
+  responseType,
+} from "@/components/utils/ProductDataTypes";
+import ProductDetail from "@/components/views/ProductDetail";
 
+// Metadata generator
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  // read route params
+  const slug = params.slug;
+  const products = await fetch(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2023-06-05/data/query/production?query=*[_type == "products"]`
+  ).then((res) => res.json());
 
+  const dataToSet: oneProductType = products.result.find(
+    (item: oneProductType) => item.slug.current === slug
+  );
 
-const Catalog =async ({params}:{params:{Slug:string}}) => {
-  return (
-    <div>slug page</div>
-  )
+  return {
+    title: dataToSet.productName,
+    description: dataToSet.description,
+  };
 }
 
-export default Catalog
+// fetched data for product in slug
+
+async function ProductData(slug: string) {
+  const res = await fetch(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2023-06-05/data/query/production?query=*%5B_type%20%3D%3D%20%22products%22%20%26%26%20slug.current%3D%3D%22{slug}%22%5D`
+  );
+
+  return res.json();
+}
+
+const Catalog = async ({ params }: { params: { slug: string } }) => {
+  let data: responseType = await ProductData(params.slug);
+  return <ProductDetail data={data.result[0]} />;
+};
+
+export default Catalog;
---
+++ src/components/views/ProductDetail/index.tsx
@@ -0,0 +1,15 @@
+
+import { FC } from "react"
+import {oneProductType} from "@/components/utils/ProductDataTypes"
+
+
+
+const ProductDetail:FC<{data:oneProductType}> = ({data}) => {
+   
+    return (
+        <div>slug</div>
+    
+    )
+  }
+  
+  export default ProductDetail;
